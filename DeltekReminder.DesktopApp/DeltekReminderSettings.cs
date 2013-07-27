419d045606910e9ba27425d5d1ab11bfcd117aaa
Lee Richardson <github@leerichardson.com>
2013-07-27 11:26:00 -0400

    Save/Retrieve settings

---
+++ DeltekReminder.DesktopApp/DeltekReminderSettings.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace DeltekReminder.DesktopApp
+{
+    [Serializable]
+    public class DeltekReminderSettings
+    {
+        private readonly object _lock = new object();
+        private const string SETTINGS_CONFIG = @"Settings.config";
+
+        public string Username { get; set; }
+        
+        public string Password { get; set; }        
+
+        public string Domain { get; set; }
+        
+        public string Url { get; set; }
+
+        private static string GetConfigFileName()
+        {
+            string path = GetDeltekReminderAppDataFolder();
+            Directory.CreateDirectory(path);
+            return Path.Combine(path, SETTINGS_CONFIG);
+        }
+
+        private static string GetDeltekReminderAppDataFolder()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Deltek Reminder");
+        }
+
+        public static DeltekReminderSettings GetSettings()
+        {
+            string fileName = GetConfigFileName();
+            return GetSettings(fileName);
+        }
+
+        private static DeltekReminderSettings GetSettings(string fileName)
+        {
+            FileStream myFileStream = null;
+            try
+            {
+                XmlSerializer mySerializer = new XmlSerializer(typeof(DeltekReminderSettings));
+                FileInfo fi = new FileInfo(fileName);
+                if (fi.Exists)
+                {
+                    myFileStream = fi.OpenRead();
+                    DeltekReminderSettings settings = (DeltekReminderSettings)mySerializer.Deserialize(myFileStream);
+                    return settings;
+                }
+            }
+            catch (Exception ex)
+            {
+                var dialogResult = MessageBox.Show("There was an error deserializing the settings file.  Click OK to revert the file and start over or cancel to close the app and fix the problem yourself.  Here's the error: " + ex, "Drat!", MessageBoxButton.OKCancel);
+                if (dialogResult == MessageBoxResult.Cancel)
+                {
+                    Application.Current.Shutdown();
+                    return null;
+                }
+            }
+            finally
+            {
+                if (myFileStream != null)
+                {
+                    myFileStream.Close();
+                }
+            }
+            DeltekReminderSettings defaultSettings = GetDefaultSettings();
+            defaultSettings.Save();
+            return defaultSettings;
+        }
+
+        private static DeltekReminderSettings GetDefaultSettings()
+        {
+            return new DeltekReminderSettings();
+        }
+
+        public virtual void Save()
+        {
+            string fileName = GetConfigFileName();
+            Save(fileName);
+        }
+
+        private void Save(string fileName)
+        {
+            lock (_lock)
+            {
+                StreamWriter myWriter = null;
+                try
+                {
+                    XmlSerializer mySerializer = new XmlSerializer(typeof(DeltekReminderSettings));
+                    myWriter = new StreamWriter(fileName, false);
+                    mySerializer.Serialize(myWriter, this);
+                }
+                finally
+                {
+                    if (myWriter != null)
+                    {
+                        myWriter.Close();
+                    }
+                }
+            }
+        }
+    }
+}
--- DeltekReminder.DesktopApp/MainWindow.xaml.cs
+++ DeltekReminder.DesktopApp/MainWindow.xaml.cs
@@ -9,29 +9,56 @@ namespace DeltekReminder.DesktopApp
     /// </summary>
     public partial class MainWindow
     {
+        private DeltekReminderSettings _settings;
+        
         public MainWindow()
         {
             InitializeComponent();
-            Login();
+
+            _settings = DeltekReminderSettings.GetSettings();
+
+            if (!string.IsNullOrEmpty(_settings.Url))
+            {
+                PopulateControlValuesFromSettings();
+                Login();
+            }
+        }
+
+        private void PopulateControlValuesFromSettings()
+        {
+            Username.Text = _settings.Username;
+            Password.Password = _settings.Password;
+            Domain.Text = _settings.Domain;
+            Url.Text = _settings.Url;
         }
 
         private void OnConnect_Click(object sender, RoutedEventArgs e)
         {
+            SaveControlValuesIntoSettings();
             Login();
+        }
+
+        private void SaveControlValuesIntoSettings()
+        {
+            _settings.Username = Username.Text;
+            _settings.Password = Password.Password;
+            _settings.Domain = Domain.Text;
+            _settings.Url = Url.Text;
+            _settings.Save();
         }
 
         private void Login()
         {
-            var source = new Uri(Url.Text);
+            var source = new Uri(_settings.Url);
             Browser.Navigate(source);
             Browser.LoadCompleted += (o, args) =>
                 {
                     if (args.Uri == source)
                     {
                         var document = (HTMLDocument)Browser.Document;
-                        SetValueById(document, "uid", Username.Text);
-                        SetValueById(document, "passField", Password.Password);
-                        SetValueById(document, "dom", Domain.Text);
+                        SetValueById(document, "uid", _settings.Username);
+                        SetValueById(document, "passField", _settings.Password);
+                        SetValueById(document, "dom", _settings.Domain);
                     }
                 };
         }
