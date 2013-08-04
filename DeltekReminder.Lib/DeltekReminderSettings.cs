13ee65eed8ce633f29b5e9da1318a4b0f61b0aaa
Lee Richardson <github@leerichardson.com>
2013-08-04 14:18:37 -0400

    I want my password to be encrypted on disk

--- DeltekReminder.Lib/DeltekReminderSettings.cs
+++ DeltekReminder.Lib/DeltekReminderSettings.cs
@@ -13,13 +13,20 @@ namespace DeltekReminder.Lib
 
         public string Username { get; set; }
         
-        public string Password { get; set; }        
+        public string EncryptedPassword { get; set; }        
 
         public string Domain { get; set; }
         
         public string BaseUrl { get; set; }
 
         public DateTime? LastSuccessfulLogin { get; set; }
+
+        [XmlIgnore]
+        public string Password
+        {
+            get { return new TripleDesStringEncryptor().DecryptString(EncryptedPassword); }
+            set { EncryptedPassword = new TripleDesStringEncryptor().EncryptString(value); }
+        }
 
         private static string GetConfigFileName()
         {
