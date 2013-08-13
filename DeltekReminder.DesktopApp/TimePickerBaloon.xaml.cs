b2cc9352d597d56c00e9431e2a07a16d69d36ce9
Lee Richardson <github@leerichardson.com>
2013-08-12 21:32:32 -0400

    Show the time picker when a user single clicks the icon

---
+++ DeltekReminder.DesktopApp/TimePickerBaloon.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeltekReminder.DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for TimePickerBaloon.xaml
+    /// </summary>
+    public partial class TimePickerBaloon : UserControl
+    {
+        public TimePickerBaloon()
+        {
+            InitializeComponent();
+        }
+    }
+}
